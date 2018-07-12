5aff3475041d8d422285cae2b0964ab21fbbe77d
djspaan <dennis@spaan.io>
2018-07-12 14:30:49 +0200

    Additional docblocks removed

--- src/main/java/DoubleStack.java
+++ src/main/java/DoubleStack.java
@@ -1,38 +1,18 @@
 public class DoubleStack implements DoubleStackInterface {
     private Stack<Double> stack = new Stack<>();
 
-    /**
-     * Pushes an element to the DoubleStack.
-     *
-     * @param element to push
-     */
     public void push(Double element) {
         stack.push(element);
     }
 
-    /**
-     * Gets and removes the first item of the DoubleStack.
-     *
-     * @return double when stack is not empty, null otherwise
-     */
     public Double pop() {
         return stack.pop();
     }
 
-    /**
-     * Gets the first item of the DoubleStack.
-     *
-     * @return double, the first item of the DoubleStack
-     */
     public Double top() {
         return stack.top();
     }
 
-    /**
-     * Gets the number of elements in the DoubleStack.
-     *
-     * @return int containing the size
-     */
     public int size() {
         return stack.size();
     }
